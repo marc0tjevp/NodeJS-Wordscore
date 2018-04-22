ada2e0f753ba511b8b2a63aed252e9093301f1ac
Marco van Poortvliet <Marc0tjevp@gmail.com>
2018-04-22 22:10:56 +0200

    Reset wordlist and simplified server code

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 var fs = require('fs');
 var express = require('express');
 var app = express();
-var server = app.listen(process.env.PORT || 3000, listening);
-
-var data = fs.readFileSync('additional.json');
-var afinndata = fs.readFileSync('afinn.json');
-var words = JSON.parse(data);
-var afinn = JSON.parse(afinndata);
-
-function listening() {
+var server = app.listen(process.env.PORT || 3000, function listening() {
   console.log('Listening...');
-}
+});
 
-app.use(express.static('website'));
+// Get Data from JSON
+var data = fs.readFileSync('wordlist.json');
+var words = JSON.parse(data);
 
-app.get('/add/:word/:score?', addWord);
+// Returns all endpoints
+app.get('/', function returnEndPoints(request, response) {
+  var reply = {
+    getAllWords: '/all',
+    searchWord: '/search/<query>',
+    addWord: '/add/<word>/<score>'
+  }
+  response.send(reply);
+});
 
-function addWord(request, response) {
+// Returns static website folder
+app.use('/gui', express.static('website'));
+
+// Adds word to json
+app.get('/add/:word/:score?', function addWord(request, response) {
   var data = request.params;
   var word = data.word;
   var score = Number(data.score);
@@ -30,7 +37,7 @@ function addWord(request, response) {
 
     words[word] = score;
     var data = JSON.stringify(words, null, 2);
-    fs.writeFile('additional.json', data, finsihed);
+    fs.writeFile('wordlist.json', data, finsihed);
 
     function finsihed(error) {
       var reply = {
@@ -41,21 +48,17 @@ function addWord(request, response) {
       response.send(reply);
     }
   }
-}
+});
 
-app.get('/all', sendAll);;
-
-function sendAll(request, response) {
+// Returns all words
+app.get('/all', function sendAll(request, response) {
   var data = {
-    additional: words,
-    afinn: afinn
+    words: words
   }
   response.send(data);
-}
+});
 
-app.get('/search/:word/', searchWord);
-
-function searchWord(request, response) {
+app.get('/search/:word/', function searchWord(request, response) {
   var word = request.params.word;
   var reply = "";
 
@@ -72,4 +75,4 @@ function searchWord(request, response) {
     }
   }
   response.send(reply);
-}
+});
